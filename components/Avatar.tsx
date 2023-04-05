5b4f2e55333202312779ba36fb8de1cd78fb3e87
Akshay <aksingh240@gmail.com>
2023-04-06 01:17:28 +0530

    added user fetch hooks

---
+++ components/Avatar.tsx
@@ -0,0 +1,11 @@
+interface AvatarProps {
+  userId: string;
+  isLarge?: boolean;
+  hasBorder?: boolean;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
+  return <></>;
+};
+
+export default Avatar;
